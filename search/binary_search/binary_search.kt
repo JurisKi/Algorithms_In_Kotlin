755ac8b833bef4a00f90b5acbddf37fcc604d447
Juris K <31712822+JurisKi@users.noreply.github.com>
2017-10-09 11:12:57 +0300

    Update binary_search.kt

--- search/binary_search/binary_search.kt
+++ search/binary_search/binary_search.kt
@@ -1,8 +1,11 @@
-//binary search
+package search
 
 import java.util.*
 
-//the iterative version of binary search
+//bianry search algorithm
+
+/*
+// The iterative version of binary search
 fun <T: Comparable<T>> binarySearch(list: List<T>, key: T): Int? {
     var rangeStart = 0
     var rangeEnd = list.count()
@@ -18,6 +21,28 @@ fun <T: Comparable<T>> binarySearch(list: List<T>, key: T): Int? {
     }
     return null
 }
+*/
+
+//the recursuive version of binary search
+fun <T: Comparable<T>> binarySearchRec(list: List<T>, key: T): Optional<Int> {
+    require(list == list.sorted())
+
+    fun helper(start: Int, end: Int): Optional<Int> {
+        val mid: Int = start + (end - start) / 2
+        if (end < start) return Optional.empty()
+
+        if (key == list[mid]) {
+            return Optional.of(mid)
+        } else if (key < list[mid]) {
+            return helper(start, mid - 1)
+        } else {
+            return helper(mid + 1, end)
+        }
+    }
+
+    return helper(0, list.count())
+}
+
 
 //test
 val numbers = listOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67)
@@ -25,6 +50,9 @@ val numbersKey = 43
 
 //this will print out 6
 fun main(args: Array<String>) {
-    val position = binarySearch(numbers, numbersKey)
+    //val position = binarySearch(numbers, numbersKey)
+    //println(position)
+    val position = binarySearchRec(numbers, numbersKey)
     println(position)
+
 }
